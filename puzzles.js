a03698e5a705f3933c048f06830c60592b29f652
unknown <gavintjhx@gmail.com>
2024-10-06 18:34:19 +0800

    remove hint

--- puzzles.js
+++ puzzles.js
@@ -2,22 +2,22 @@ const puzzlePieces = [
     { 
         // Top left piece - MAC
         hash: "tl-14057263d5a894e23277311e6a211c3196d1c764041c4cde6ec947adb0a4fa8672f758898ca2d24179e4fb9dbfbff6425f4b13f2786c601ae338ed57a9dd20fa",
-        question: "[TL] Computers are hospitalized and recover fully before leaving this place."
+        question: ""
     },
     { 
         // Top right piece - WSS (Level 6)
         hash: "tr-c38f308b233f1dc5f63bca1e48119f275e62e3752d3e928caf1d0c679b9021c0544dae269b08ec76225f1a93994db57c5d1114b488c476c0f5b06851cc57152f",
-        question: "[TR] Room for the 'elites'."
+        question: ""
     },
     { 
         // Bottom left piece - Level 8 ceiling
         hash: "bl-d8cca31f0572e74a31ff48b9441db3888b787e148d2a57aefbf20edb1a4b13b0bb54144a2c631411eaef6092df6a38e3a40d967013b509297d5c78d735a56895",
-        question: "[BL] Somewhere the OSI Model cannot reach."
+        question: ""
     },
     { 
         // Bottom right piece - HSS Vending Machine
         hash: "br-71dad2d4ee272cf4179fa651f32d43c517d9944e80d41ed1d899e7601b4aa9a26221febda557f1497b4f32ebbb00fda30df8ad3cdddbbdd2915607d3466b9937",
-        question: "[BR] Do these machines belong in HSS or IIT..? Do they even make money?"
+        question: ""
     },
     {
         /*
